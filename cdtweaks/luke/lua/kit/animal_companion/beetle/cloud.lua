c150720d46c380dff1c217bced2e7c2d09ebb10b
4Luke4 <luke9300@icloud.com>
2025-04-06 19:25:08 +0200

    Update creatures traits

---
+++ cdtweaks/luke/lua/kit/animal_companion/beetle/cloud.lua
@@ -0,0 +1,72 @@
+--[[
++------------------------------------------------------------------------------------------------------------------+
+| Bombardier Beetle Cloud: targeted creature is either stunned for 2d4 rounds or deafened for 2d6 rounds (no save) |
++------------------------------------------------------------------------------------------------------------------+
+--]]
+
+function %INNATE_BOMBARDIER_BEETLE_CLOUD%(CGameEffect, CGameSprite)
+	local stunDuration = math.random(4) + math.random(4) -- 2d4
+	local deafDuration = math.random(6) + math.random(6) -- 2d6
+	--
+	local stunImmunity = EEex_Trigger_ParseConditionalString("EEex_IsImmuneToOpcode(Myself,45)")
+	local deafImmunity = EEex_Trigger_ParseConditionalString("EEex_IsImmuneToOpcode(Myself,80)")
+	--
+	if not stunImmunity:evalConditionalAsAIBase(CGameSprite) then
+		if math.random(5) == 1 then -- 20% chance
+			EEex_GameObject_ApplyEffect(CGameSprite,
+			{
+				["effectID"] = 45, -- Stun
+				["duration"] = 6 * stunDuration,
+				["m_sourceRes"] = CGameEffect.m_sourceRes:get(),
+				["m_sourceType"] = CGameEffect.m_sourceType,
+				["sourceID"] = CGameEffect.m_sourceId,
+				["sourceTarget"] = CGameEffect.m_sourceTarget,
+			})
+			EEex_GameObject_ApplyEffect(CGameSprite,
+			{
+				["effectID"] = 139, -- Display String
+				["effectAmount"] = %feedback_strref_stun%,
+				["m_sourceRes"] = CGameEffect.m_sourceRes:get(),
+				["m_sourceType"] = CGameEffect.m_sourceType,
+				["sourceID"] = CGameEffect.m_sourceId,
+				["sourceTarget"] = CGameEffect.m_sourceTarget,
+			})
+		end
+	end
+	--
+	if not deafImmunity:evalConditionalAsAIBase(CGameSprite) then
+		if math.random(5) == 1 then -- 20% chance
+			EEex_GameObject_ApplyEffect(CGameSprite,
+			{
+				["effectID"] = 80, -- Deafness
+				["duration"] = 6 * deafDuration,
+				["m_sourceRes"] = CGameEffect.m_sourceRes:get(),
+				["m_sourceType"] = CGameEffect.m_sourceType,
+				["sourceID"] = CGameEffect.m_sourceId,
+				["sourceTarget"] = CGameEffect.m_sourceTarget,
+			})
+			EEex_GameObject_ApplyEffect(CGameSprite,
+			{
+				["effectID"] = 142, -- Icon
+				["duration"] = 6 * deafDuration,
+				["dwFlags"] = 112,
+				["m_sourceRes"] = CGameEffect.m_sourceRes:get(),
+				["m_sourceType"] = CGameEffect.m_sourceType,
+				["sourceID"] = CGameEffect.m_sourceId,
+				["sourceTarget"] = CGameEffect.m_sourceTarget,
+			})
+			EEex_GameObject_ApplyEffect(CGameSprite,
+			{
+				["effectID"] = 139, -- Display String
+				["effectAmount"] = %feedback_strref_deaf%,
+				["m_sourceRes"] = CGameEffect.m_sourceRes:get(),
+				["m_sourceType"] = CGameEffect.m_sourceType,
+				["sourceID"] = CGameEffect.m_sourceId,
+				["sourceTarget"] = CGameEffect.m_sourceTarget,
+			})
+		end
+	end
+	--
+	stunImmunity:free()
+	deafImmunity:free()
+end
---
+++ cdtweaks/luke/lua/kit/animal_companion/leopard/sneak_attack.lua
@@ -0,0 +1,76 @@
+--[[
++--------------------------------------------------------------------------------------------------+
+| Leopard: Can perform sneak attacks as if it was an assassin of the same level (see sneakatt.2da) |
++--------------------------------------------------------------------------------------------------+
+--]]
+
+function %INNATE_LEOPARD_SNEAK_ATTACK%(CGameEffect, CGameSprite)
+	if CGameEffect.m_effectAmount == 1 then -- check if can perform a sneak attack
+		local sourceSprite = EEex_GameObject_Get(CGameEffect.m_sourceId)
+		--
+		local targetActiveStats = EEex_Sprite_GetActiveStats(CGameSprite)
+		-- limit to once per round
+		local conditionalString = EEex_Trigger_ParseConditionalString('!GlobalTimerNotExpired("gtLeopardSneakattTimer","LOCALS")')
+		local responseString = EEex_Action_ParseResponseString('SetGlobalTimer("gtLeopardSneakattTimer","LOCALS",6)')
+		--
+		if conditionalString:evalConditionalAsAIBase(sourceSprite) then
+			-- if the target is incapacitated || the target is in combat with someone else || the leopard is invisible
+			if EEex_BAnd(targetActiveStats.m_generalState, 0x100029) ~= 0 or CGameSprite.m_targetId ~= sourceSprite.m_id or sourceSprite:getLocalInt("gtSpriteIsInvisible") == 1 then
+				responseString:executeResponseAsAIBaseInstantly(sourceSprite)
+				--
+				CGameSprite:applyEffect({
+					["effectID"] = 146, -- Cast spell
+					["res"] = "%INNATE_LEOPARD_SNEAK_ATTACK%", -- SPL file
+					["dwFlags"] = 1, -- cast instantly / ignore level
+					["sourceID"] = CGameEffect.m_sourceId,
+					["sourceTarget"] = CGameEffect.m_sourceTarget,
+				})
+			end
+		end
+		--
+		responseString:free()
+		conditionalString:free()
+	elseif CGameEffect.m_effectAmount == 2 then -- actual sneak attack
+		local sneakatt = GT_Resource_2DA["sneakatt"]
+		--
+		local sourceSprite = EEex_GameObject_Get(CGameEffect.m_sourceId)
+		local sourceActiveStats = EEex_Sprite_GetActiveStats(sourceSprite)
+		--
+		local equipment = sourceSprite.m_equipment -- CGameSpriteEquipment
+		local selectedWeapon = equipment.m_items:get(equipment.m_selectedWeapon) -- CItem
+		local selectedWeaponHeader = selectedWeapon.pRes.pHeader -- Item_Header_st
+		local selectedWeaponAbility = EEex_Resource_GetItemAbility(selectedWeaponHeader, equipment.m_selectedWeaponAbility) -- Item_ability_st
+		--
+		local immunityToDamage = EEex_Trigger_ParseConditionalString("EEex_IsImmuneToOpcode(Myself,12)")
+		--
+		local targetActiveStats = EEex_Sprite_GetActiveStats(CGameSprite)
+		--
+		local op12DamageType, ACModifier = GT_Utility_DamageTypeConverter(selectedWeaponAbility.damageType, targetActiveStats)
+		--
+		if not immunityToDamage:evalConditionalAsAIBase(CGameSprite) then
+			EEex_GameObject_ApplyEffect(CGameSprite,
+			{
+				["effectID"] = 0xC, -- Damage
+				["dwFlags"] = op12DamageType * 0x10000, -- mode: normal
+				["numDice"] = tonumber(sneakatt["ASSASIN"][string.format("%s", sourceActiveStats.m_nLevel1)]), -- typo in KIT.IDS / KITLIST.2DA
+				["diceSize"] = 6,
+				["m_sourceRes"] = CGameEffect.m_sourceRes:get(),
+				["m_sourceType"] = CGameEffect.m_sourceType,
+				["sourceID"] = CGameEffect.m_sourceId,
+				["sourceTarget"] = CGameEffect.m_sourceTarget,
+			})
+		else
+			EEex_GameObject_ApplyEffect(CGameSprite,
+			{
+				["effectID"] = 324, -- Immunity to resource and message
+				["res"] = CGameEffect.m_sourceRes:get(),
+				["m_sourceRes"] = CGameEffect.m_sourceRes:get(),
+				["m_sourceType"] = CGameEffect.m_sourceType,
+				["sourceID"] = CGameEffect.m_sourceId,
+				["sourceTarget"] = CGameEffect.m_sourceTarget,
+			})
+		end
+		--
+		immunityToDamage:free()
+	end
+end
