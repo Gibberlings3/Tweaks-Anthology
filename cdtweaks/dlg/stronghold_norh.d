48e769566d29922b7cb002a03adac5f93c91f656
CamDawg <pcamagna@yahoo.com>
2025-04-22 15:20:55 -0700

    stronghold bug
    De'arnise bit of strongholds was modifying paladin stuff

--- cdtweaks/dlg/stronghold_norh.d
+++ cdtweaks/dlg/stronghold_norh.d
@@ -3,3 +3,8 @@ REPLACE_TRIGGER_TEXT ~garren~   ~Kit(Player1,Blackguard)~ ~False()~ // bg2ee
 
 REPLACE_TRIGGER_TEXT ~hprelate~ ~Class(Player1,PALADIN\(_[A-Z]+\)?)~ ~True()~
 REPLACE_TRIGGER_TEXT ~hprelate~ ~Kit(Player1,Blackguard)~ ~False()~ // bg2ee
+
+ADD_TRANS_ACTION demson 
+BEGIN 119 END
+BEGIN END
+~EraseJournalEntry(22917)~
